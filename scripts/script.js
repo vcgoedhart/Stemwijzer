9b0d64e525e33e495c809d549b3b8a5f7c290738
Vincent Goedhart <vcgoedhart@hotmail.com>
2020-02-19 00:56:59 +0100

    Old

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,25 @@
 var currentQuestion = 0;
-
-showContainer = function (element) {
-    var containers = document.getElementsByClassName("containers");
-    for (var container of containers) {
-        container.classList.add("d-none");
-    }
-    element.classList.remove("d-none");
-}
-
-function init() {
-    for (var party of parties) party.votes = 0;
-}
+for (var party of parties) party.votes = 0;
 
 function start() {
     showContainer(document.getElementById("question-container"));
     setQuestion();
 }
 
-function setQuestion() {
-    if (subjects[currentQuestion]) {
-         document.getElementById("questionTitle").innerHTML = (currentQuestion + 1) + ". " + subjects[currentQuestion].title;
-         document.getElementById("questionBox").innerHTML = subjects[currentQuestion].statement;
+function next() {
+    currentQuestion--;
+    setQuestion(currentQuestion)
+}
+
+function back() {
+    currentQuestion--;
+    setQuestion(currentQuestion)
+}
+
+function setQuestion(value) {
+    if (subjects[value]) {
+        document.getElementById("questionTitle").innerHTML = (value + 1) + ". " + subjects[value].title;
+        document.getElementById("questionBox").innerHTML = subjects[value].statement;
     } else {
         setResults(document.getElementById("result-container"));
         showContainer(document.getElementById("result-container"));
@@ -29,51 +28,52 @@ function setQuestion() {
 
 function saveQuestion(answer){
     // Adds votes to the parties with the same answer
-    for (var subjectParty of subjects[currentQuestion].parties){
-        if (subjectParty.position === answer){
-            for (var party of parties) {
-                if (party.name === subjectParty.name){
-                    party.votes++;
-                }
-            }
-        }
-    }
+    // for (var subjectParty of subjects[currentQuestion].parties){
+    //     if (subjectParty.position === answer){
+    //         for (var party of parties) {
+    //             if (party.name === subjectParty.name){
+    //                 party.votes++;
+    //             }
+    //         }
+    //     }
+    // }
+    subjects.answer = answer;
 
     // Next question
     currentQuestion++;
-    setQuestion()
+    setQuestion(currentQuestion)
 }
 
-function setResults(element) {   
-    var votes = [],
-        totalVotes = 0;
+// function setResults(element) {   
+//     var votes = [],
+//         totalVotes = 0;
 
-    console.log(parties);
-    for (var party of parties){
-        votes.push(party.votes + ":" + party.name);
-        totalVotes += party.votes;
-    }
-    console.log(totalVotes, votes);
-    votes.sort().reverse();
-    for (var i = 0; i < 3; i++) {
-        var meta = votes[i].split(":");
+//     console.log(parties);
+//     for (var party of parties){
+//         votes.push(party.votes + ":" + party.name);
+//         totalVotes += party.votes;
+//     }
+//     console.log(totalVotes, votes);
+//     votes.sort().reverse();
+//     for (var i = 0; i < 3; i++) {
+//         var meta = votes[i].split(":");
 
-        var partyVotes = Number(meta[0]);
-        var partyName = meta[1];
+//         var partyVotes = Number(meta[0]);
+//         var partyName = meta[1];
 
-        var p = document.createElement("p");
+//         var p = document.createElement("p");
 
-        if (totalVotes > 0){
-            var matchPercentage = (partyVotes / totalVotes) * 100;
-            var textNode = document.createTextNode("Partij: " + partyName + ", Stemmen: " + matchPercentage + "%");
-            p.appendChild(textNode);
-            element.appendChild(p);
-        } else {
-            var textNode = document.createTextNode(":(");
-            p.appendChild(textNode);
-            element.appendChild(p);
-            return;
-        }
-    }
-    console.log(votes);
-}
+//         if (totalVotes > 0){
+//             var matchPercentage = (partyVotes / totalVotes) * 100;
+//             var textNode = document.createTextNode("Partij: " + partyName + ", Stemmen: " + Math.round(matchPercentage) + "%");
+//             p.appendChild(textNode);
+//             element.appendChild(p);
+//         } else {
+//             var textNode = document.createTextNode(":(");
+//             p.appendChild(textNode);
+//             element.appendChild(p);
+//             return;
+//         }
+//     }
+//     console.log(votes);
+// }
