8be89fe12cfa77f64a9bbe857c97baed68217f17
Vincent Goedhart <vcgoedhart@hotmail.com>
2020-02-20 22:38:13 +0100

    Added file for common functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,79 +1,20 @@
-var currentQuestion = 0;
-for (var party of parties) party.votes = 0;
-
 function start() {
-    showContainer(document.getElementById("question-container"));
-    setQuestion();
+    setQuestion(0);
 }
 
-function next() {
-    currentQuestion--;
-    setQuestion(currentQuestion)
-}
-
-function back() {
-    currentQuestion--;
-    setQuestion(currentQuestion)
-}
-
-function setQuestion(value) {
-    if (subjects[value]) {
-        document.getElementById("questionTitle").innerHTML = (value + 1) + ". " + subjects[value].title;
-        document.getElementById("questionBox").innerHTML = subjects[value].statement;
+function setWeight() {
+    var checkbox = document.getElementById("checkbox-weight");
+    if (checkbox.checked) {
+        subjects[currentQuestion].weight = true;
     } else {
-        setResults(document.getElementById("result-container"));
-        showContainer(document.getElementById("result-container"));
+        subjects[currentQuestion].weight = null;
     }
 }
 
-function saveQuestion(answer){
-    // Adds votes to the parties with the same answer
-    // for (var subjectParty of subjects[currentQuestion].parties){
-    //     if (subjectParty.position === answer){
-    //         for (var party of parties) {
-    //             if (party.name === subjectParty.name){
-    //                 party.votes++;
-    //             }
-    //         }
-    //     }
-    // }
-    subjects.answer = answer;
-
-    // Next question
-    currentQuestion++;
-    setQuestion(currentQuestion)
+function loadCheckbox(value) {
+    if (subjects[value].weight) {
+        document.getElementById("checkbox-weight").checked = true;
+    } else {
+        document.getElementById("checkbox-weight").checked = false;
+    }
 }
-
-// function setResults(element) {   
-//     var votes = [],
-//         totalVotes = 0;
-
-//     console.log(parties);
-//     for (var party of parties){
-//         votes.push(party.votes + ":" + party.name);
-//         totalVotes += party.votes;
-//     }
-//     console.log(totalVotes, votes);
-//     votes.sort().reverse();
-//     for (var i = 0; i < 3; i++) {
-//         var meta = votes[i].split(":");
-
-//         var partyVotes = Number(meta[0]);
-//         var partyName = meta[1];
-
-//         var p = document.createElement("p");
-
-//         if (totalVotes > 0){
-//             var matchPercentage = (partyVotes / totalVotes) * 100;
-//             var textNode = document.createTextNode("Partij: " + partyName + ", Stemmen: " + Math.round(matchPercentage) + "%");
-//             p.appendChild(textNode);
-//             element.appendChild(p);
-//         } else {
-//             var textNode = document.createTextNode(":(");
-//             p.appendChild(textNode);
-//             element.appendChild(p);
-//             return;
-//         }
-//     }
-//     console.log(votes);
-// }
