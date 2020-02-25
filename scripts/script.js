3a755be90f88b66a8401718f218b47a1b8f0d10f
Vincent Goedhart <vcgoedhart@hotmail.com>
2020-02-25 02:59:51 +0100

    Added setOpinions(x) to load all the opinions

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 function start() {
-    setQuestion(0);
+    loadQuestion(0);
 }
 
 function setWeight() {
@@ -11,12 +11,50 @@ function setWeight() {
     }
 }
 
-function loadCheckbox(value) {
+function setCheckbox(value) {
     if (subjects[value].weight) {
         document.getElementById("checkbox-weight").checked = true;
     } else {
         document.getElementById("checkbox-weight").checked = false;
     }
+}
+
+function setOpinions(value) {
+    var container = document.getElementsByClassName("opinion-container");
+    for (var c of container) {
+        c.innerHTML = "";
+    }
+
+    for (var party of subjects[value].parties) {
+        if (party.position === "pro") {
+            container = document.getElementById("opinion-eens-container");
+        } else if (party.position === "contra") {
+            container = document.getElementById("opinion-oneens-container");
+        } else {
+            container = document.getElementById("opinion-none-container");
+        }
+
+        var paragraph = createElement("p", { "class": "mb-1" }, party.name);
+        var span = createElement("span", { "class": "text-muted" }, party.opinion);
+        var hr = document.createElement("hr");
+
+        paragraph.style.cursor = "pointer";
+        span.style.fontSize = "14px";
+        span.hidden = true;
+
+        paragraph.onclick = function () {
+            if (this.nextSibling.hidden) {
+                this.nextSibling.hidden = false;
+            } else {
+                this.nextSibling.hidden = true;
+            }
+        }
+
+        container.appendChild(paragraph);
+        container.appendChild(span);
+        container.appendChild(hr);
+    }
+
 }
 
 function filter(element, filterType) {
